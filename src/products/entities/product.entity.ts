0f290d8e6209b4e94db54535264c1c2310a91c34
dani-agudelo <daniiagudelom@gmail.com>
2025-10-20 21:47:05 -0500

    feat: se añade validación de .env con joi

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,12 +1,25 @@
+interface UpdateWithOptions {
+    name?: string,
+    description?: string,
+    price?: number
+}
+
+
 export class Product {
-    
+
 
     constructor(
         public id: string,
         public name: string,
         public price: number,
         public description: string,
-    ) {}
+    ) { }
 
     //TODO : UPDATE WITH
+
+    updateWith({ name, description, price }: UpdateWithOptions) {
+        this.name = name ?? this.name;
+        this.description = description ?? this.description;
+        this.price = price ?? this.price
+    }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,30 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { v4 as Uuidv4 } from 'uuid';
+
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  
-  private products: Product[]= [];
-  
+
+  private products: Product[] = [];
+
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    const { name, description, price } = createProductDto;
+
+    const newProduct = new Product(
+      Uuidv4(),
+      name,
+      price,
+      description,
+    );
+    this.products.push(newProduct);
+    return newProduct;
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.products;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  findOne(id: string) {
+    const product = this.products.find(item => item.id === id.toString());
+    if (!product) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  update(id: string, updateProductDto: UpdateProductDto) {
+    const { id: _, ...values } = updateProductDto;
+
+    const product = this.findOne(id);
+    product.updateWith(updateProductDto);
+    return product;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  remove(id: string) {
+    const product = this.findOne(id);
+    this.products = this.products.filter(item => item.id !== id);
+    return product;
   }
 }
